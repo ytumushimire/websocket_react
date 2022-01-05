885f4b4a060cb6556dbf052f8689e6f0ea982cbe
Yves Tumushimire <ytumushimire@truststamp.net>
2022-01-05 11:11:17 +0200

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import W3CWebSocket from 'websocket'
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    const ws = new W3CWebSocket.w3cwebsocket('ws://172.16.20.241:8000/ws/statistics/?api_key=SHPCzFqb.1Wi9Mkn3VPQuT3SQ6F5Y1EhzFeYaVW4O')
+    ws.onopen = () => {
+      ws.send(JSON.stringify({ 'api_key': 'SHPCzFqb.1Wi9Mkn3VPQuT3SQ6F5Y1EhzFeYaVW4O' }))
+      console.log('connection open')
+    }
+    ws.onerror = (e) => {
+      console.log(e)
+    }
+    ws.onclose = () => {
+      console.log('connection close')
+    }
+
+    ws.onmessage = (e) => {
+      // const data = JSON.parse(e.data)
+      // const { type, payload } = data
+      console.log(JSON.parse(e.data))
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
 }
-
-export default App;
